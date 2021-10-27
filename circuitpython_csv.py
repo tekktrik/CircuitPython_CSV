b46172924a3f2222f0052602d09156514cff8258
Alec Delaney <tekktrik@gmail.com>
2021-10-27 12:12:54 -0400

    Add reader class prototype

--- circuitpython_csv.py
+++ circuitpython_csv.py
@@ -33,3 +33,45 @@ Implementation Notes
 
 # imports__version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/tekktrik/Circuitpython_CircuitPython_CSV.git"
+
+import io
+import re
+
+class reader:
+    
+    def __init__(self, file_iterator: io.TextIOWrapper, delimiter: str = ',', quotechar: str = '"'):
+
+        self.file_interator = file_iterator
+        self.delimiter = delimiter
+        self.quotechar = quotechar
+        self._re_exp = '(\\' + quotechar + '.+?\\' + quotechar + ')|([^' + delimiter + ']+)'
+
+    def __iter__(self):
+        return self.file_interator
+
+    def __next__(self):
+        csv_value_list = []
+        pre_regex_string = self.file_interator.__next__()
+
+        while len(pre_regex_string) != 0: # while length of string is not zero
+
+            if pre_regex_string.startswith(self.delimiter): # if string starts with delimiter, add element and remove
+                csv_value_list.append('')
+                pre_regex_string = pre_regex_string[1:]
+                continue
+
+            next_match = re.match(self._re_exp, pre_regex_string) # get text match, and add to list
+            for match in next_match.groups():
+                if match != None:
+                    csv_value_list.append(match)
+                    break
+
+            last_index = next_match.span()[1] # remove the match from the string
+
+            if len(pre_regex_string) != 0: # If anything is left in the list...
+                pre_regex_string = pre_regex_string[last_index:]
+                if pre_regex_string == self.delimiter: # if all that's left is a trailing delimiter, remove and append element to list
+                    csv_value_list.append('')
+                    pre_regex_string = pre_regex_string[1:]
+
+        return csv_value_list
