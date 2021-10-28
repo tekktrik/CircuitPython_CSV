45f2455841780f61faa8df0380d136bdd4e04c75
Alec Delaney <tekktrik@gmail.com>
2021-10-28 09:40:27 -0400

    Hardware links not required

--- circuitpython_csv.py
+++ circuitpython_csv.py
@@ -16,8 +16,7 @@ Implementation Notes
 
 **Hardware:**
 
-.. todo:: Add links to any specific hardware product page(s), or category page(s).
-  Use unordered list & hyperlink rST inline format: "* `Link Text <url>`_"
+None
 
 **Software and Dependencies:**
 
