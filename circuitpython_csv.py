8bc426b0f80ffdf518775c6451bedfc7e5800c15
Alec Delaney <tekktrik@gmail.com>
2021-10-28 09:40:42 -0400

    Bus and Register links not required

--- circuitpython_csv.py
+++ circuitpython_csv.py
@@ -23,11 +23,6 @@ None
 * Adafruit CircuitPython firmware for the supported boards:
   https://github.com/adafruit/circuitpython/releases
 
-.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies
-  based on the library's use of either.
-
-# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
-# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
 """
 
 # imports__version__ = "0.0.0-auto.0"
