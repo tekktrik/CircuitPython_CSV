4ed497b39a25582b8e2077a0b311d77525b9a7cc
Alec Delaney <tekktrik@gmail.com>
2021-10-28 12:42:46 -0400

    Updated to match CPython functionality, ported/copied DictReader and DictWriter class
    Tweaked slightly where appropriate

--- circuitpython_csv.py
+++ circuitpython_csv.py
@@ -31,16 +31,16 @@ __repo__ = "https://github.com/tekktrik/Circuitpython_CircuitPython_CSV.git"
 import re
 
 try:
-    from typing import List
+    from typing import List, Optional, Any, Dict
     import io
 except ImportError:
     pass
 
 class reader:
 
-    def __init__(self, file_iterator: io.TextIOWrapper, delimiter: str = ',', quotechar: str = '"'):
+    def __init__(self, csvfile: io.TextIOWrapper, delimiter: str = ',', quotechar: str = '"'):
 
-        self.file_interator = file_iterator
+        self.file_interator = csvfile
         self.delimiter = delimiter
         self.quotechar = quotechar
         self._re_exp = '(\\' + quotechar + '.+?\\' + quotechar + ')|([^' + delimiter + ']+)'
@@ -81,9 +81,9 @@ class reader:
 
 class writer:
 
-    def __init__(self, file_iterator: io.TextIOWrapper, delimiter: str = ',', quoterchar: str = '"'):
+    def __init__(self, csvfile: io.TextIOWrapper, delimiter: str = ',', quoterchar: str = '"'):
 
-        self.file_iterator = file_iterator
+        self.file_iterator = csvfile
         self.delimiter = delimiter
         self.quotechar = quoterchar
         self.newlinechar = '\r\n'
@@ -94,6 +94,72 @@ class writer:
         parsed_str = (self.delimiter).join(parsed_seq)
         self.file_iterator.write(parsed_str + self.newlinechar)
 
+    def writerows(self, rows: iter):
+        for row in rows:
+            self.writerow(row)
+
     def _apply_quotes(self, entry):
 
         return (self.quotechar + entry + self.quotechar) if self.delimiter in entry else entry
+
+# Mostly copied from CPython's csv.py:
+class DictReader:
+
+    def __init__(self, f: io.TextIOWrapper, fieldnames: Optional[List] = None, restkey: Optional[str] = None, restval: Optional[Any] = None, *args, **kwargs):
+
+        self.fieldnames = fieldnames
+        self.restkey = restkey
+        self.restval = restval
+        self.reader = reader(f, *args, **kwargs)
+        self.line_num = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.line_num == 0:
+            if self.fieldnames == None:
+                self.fieldnames = next(self.reader)
+        row = next(self.reader)
+
+        d = dict(zip(self.fieldnames, row))
+        lf = len(self.fieldnames)
+        lr = len(row)
+        if lf < lr:
+            d[self.restkey] = row[lf:]
+        elif lf > lr:
+            for key in self.fieldnames[lr:]:
+                d[key] = self.restval
+        self.line_num += 1
+        return d
+
+
+# Copied from CPython's csv.py
+class DictWriter:
+
+    def __init__(self, f: io.TextIOWrapper, fieldnames: List, restval: str = "", extrasaction: str = "raise", *args, **kwds):
+        self.fieldnames = fieldnames    # list of keys for the dict
+        self.restval = restval          # for writing short dicts
+        if extrasaction.lower() not in ("raise", "ignore"):
+            raise ValueError("extrasaction ""(%s)"" must be 'raise' or 'ignore'"
+                             % extrasaction)
+        self.extrasaction = extrasaction
+        self.writer = writer(f, *args, **kwds)
+
+    def writeheader(self):
+        header = dict(zip(self.fieldnames, self.fieldnames))
+        return self.writerow(header)
+
+    def _dict_to_list(self, rowdict: Dict):
+        if self.extrasaction == "raise":
+            wrong_fields = rowdict.keys() - self.fieldnames
+            if wrong_fields:
+                raise ValueError("dict contains fields not in fieldnames: "
+                                 + ", ".join([repr(x) for x in wrong_fields]))
+        return (rowdict.get(key, self.restval) for key in self.fieldnames)
+
+    def writerow(self, rowdict: Dict):
+        return self.writer.writerow(self._dict_to_list(rowdict))
+
+    def writerows(self, rowdicts: iter):
+        return self.writer.writerows(map(self._dict_to_list, rowdicts))
